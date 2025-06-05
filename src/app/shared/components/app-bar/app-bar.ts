23d64900e322de37cccd9bfa30f8eb906bf5e97a
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-05 16:50:43 -0400

    feat(web): :sparkles: Hide app bar on non auth routes

--- src/app/shared/components/app-bar/app-bar.ts
+++ src/app/shared/components/app-bar/app-bar.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { heroHomeSolid, heroPlaySolid, heroShoppingCartSolid } from '@ng-icons/heroicons/solid';
 import { Button } from '../button/button';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-app-bar',
@@ -9,4 +10,29 @@ import { Button } from '../button/button';
   templateUrl: './app-bar.html',
   viewProviders: [provideIcons({ heroHomeSolid, heroPlaySolid, heroShoppingCartSolid })]
 })
-export class AppBar {}
+export class AppBar implements OnInit {
+  private readonly router = inject(Router);
+  private readonly activatedRoute = inject(ActivatedRoute);
+
+  protected display = signal<boolean>(false);
+
+  public ngOnInit(): void {
+    // Subscribe to NavigationEnd router events (when a redirect or navigation is completed)
+    this.router.events.subscribe((event) => {
+      if (event.constructor.name === 'NavigationEnd') {
+        // Get the current activated route
+        const currentRoute = this.activatedRoute.root;
+
+        // Get the route information (data)
+        const routeData = currentRoute.firstChild?.snapshot.data;
+
+        // Determine if the route required authentication
+        const requiresAuth = routeData?.['needsAuth'] ?? false;
+
+        // If the route requires authentication, display the app bar
+        // Otherwise, hide the app bar
+        this.display.set(requiresAuth);
+      }
+    });
+  }
+}

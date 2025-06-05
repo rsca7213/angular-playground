e106910afdd6b6d1602b3e419b1f324bccce2799
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-05 16:15:59 -0400

    chore(web): :wrench: Add config for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,23 @@ module.exports = {
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [],
+  safelist: [
+    'bg-primary',
+    'bg-secondary',
+    'bg-success',
+    'bg-danger',
+    'bg-warning',
+    'bg-info',
+    'bg-light',
+    'bg-dark',
+    'text-primary',
+    'text-secondary',
+    'text-success',
+    'text-danger',
+    'text-warning',
+    'text-info',
+    'text-light',
+    'text-dark'
+  ]
 };

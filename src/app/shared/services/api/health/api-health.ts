73d35b6b223d48dea0b4279b52959f1cf449a1ca
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-06 13:25:41 -0400

    refactor(web): :truck: Move api health service

---
+++ src/app/shared/services/api/health/api-health.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ENVIRONMENT } from '../../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { IApiHealthCheckResponse } from '../../../dtos/api/health/api-health-check-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiHealth {
+  private readonly http = inject(HttpClient);
+
+  public constructor() {}
+
+  public check(): Observable<IApiHealthCheckResponse> {
+    return this.http.get<IApiHealthCheckResponse>(`${ENVIRONMENT.apiUrl}/actuator/health`);
+  }
+}

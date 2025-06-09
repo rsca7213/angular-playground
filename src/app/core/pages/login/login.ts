da31cf7dade902b86d562673555afc933fed86e9
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-09 17:51:24 -0400

    feat(web): :sparkles: Fully functional login component view

--- src/app/core/pages/login/login.ts
+++ src/app/core/pages/login/login.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormInput } from '../../../shared/ui/form-input/form-input';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Button } from '../../../shared/ui/button/button';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroArrowRightEndOnRectangleSolid, heroLockClosedSolid } from '@ng-icons/heroicons/solid';
+import { FormUtils } from '../../../shared/ui/form-utils/form-utils';
+import { ApiAuth } from '../../../shared/services/api/auth/api-auth';
+import { Alert } from '../../../shared/ui/alert/alert';
+import { Router } from '@angular/router';
+import { Card } from '../../../shared/ui/card/card';
+import { heroExclamationTriangle } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html'
+  imports: [FormInput, Button, ReactiveFormsModule, NgIcon, Alert, Card],
+  templateUrl: './login.html',
+  providers: [
+    provideIcons({
+      heroArrowRightEndOnRectangleSolid,
+      heroLockClosedSolid,
+      heroExclamationTriangle
+    })
+  ]
 })
-export class Login {}
+export class Login extends FormUtils {
+  private readonly apiAuth = inject(ApiAuth);
+  private readonly router = inject(Router);
+
+  protected readonly form = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [Validators.required, Validators.minLength(10)])
+  });
+
+  protected override async submitLogic(): Promise<void> {
+    // Perform the login operation using the API service
+    const result = await this.apiAuth.login({
+      email: this.form.value.email!,
+      password: this.form.value.password!
+    });
+
+    // Check if the result is an error response
+    if ('errorCode' in result) {
+      this.submissionError.set(result.message);
+      return;
+    }
+
+    // If login is successful, redirect to the home page
+    this.router.navigate(['/']);
+  }
+}

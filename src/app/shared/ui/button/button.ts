1fde2574e152980181eb9e12de050a44a8684c04
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-09 17:48:27 -0400

    feat(web): :sparkles: Add button disabled and loading state

--- src/app/shared/ui/button/button.ts
+++ src/app/shared/ui/button/button.ts
@@ -1,24 +1,61 @@
-import { Component, computed, input } from '@angular/core';
+import { booleanAttribute, Component, computed, input } from '@angular/core';
 import { TAppColors } from '../../types/colors';
 import { colorContrast } from '../../constants/color-contrast';
 import { RouterLink } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroArrowPathSolid } from '@ng-icons/heroicons/solid';
+import { TButtonType } from '../../types/buttons';
 
 @Component({
   selector: 'app-button',
-  imports: [RouterLink],
-  templateUrl: './button.html'
+  imports: [RouterLink, NgIcon],
+  templateUrl: './button.html',
+  providers: [provideIcons({ heroArrowPathSolid })]
 })
 export class Button {
-  readonly defaultClasses =
-    'rounded-lg text-md py-2 px-3 transition-colors duration-200 cursor-pointer';
+  // Button configuration properties
+  readonly type = input<TButtonType>('button');
   readonly title = input.required<string>();
   readonly color = input.required<TAppColors>();
   readonly redirectTo = input<string | string[] | null>(null);
+  readonly full = input(false, { transform: booleanAttribute });
+  readonly disabled = input(false, { transform: booleanAttribute });
+  readonly loading = input(false, { transform: booleanAttribute });
 
-  // Auto-generate button classes
-  protected buttonClasses = computed(
-    () => `bg-${this.color()} text-${this.getTextColor()} ${this.defaultClasses}`
-  );
+  private readonly defaultClasses =
+    'rounded-lg text-md py-2 px-3 transition-colors duration-200 cursor-pointer flex items-center justify-center';
+
+  // Auto-generate button classes using an array for better readability
+  protected buttonClasses = computed(() => {
+    let classes: string[] = [
+      ...this.defaultClasses.split(' '),
+      `bg-${this.color()}`,
+      `text-${this.getTextColor()}`
+    ];
+
+    // Determine if button should take up full width
+    if (this.full()) {
+      classes.push('w-full');
+    }
+
+    // Add loading state class
+    if (this.loading()) {
+      classes.push('opacity-50');
+      classes.push('cursor-wait');
+      classes = classes.filter((c) => c !== 'cursor-pointer'); // Remove pointer cursor when loading
+    }
+
+    // Add disabled state class
+    if (this.disabled()) {
+      classes.push('opacity-50');
+      classes.push('cursor-not-allowed');
+      classes = classes.filter((c) => c !== 'cursor-pointer'); // Remove pointer cursor when disabled
+    }
+
+    //
+
+    return classes.join(' ');
+  });
 
   private getTextColor = () => colorContrast.get(this.color()) || 'dark';
 }

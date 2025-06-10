03c55a111c2536cf09c854ae7a869b810b10675c
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-10 11:54:34 -0400

    chore(web): :wrench: Add Angular proxy to be able to use HttpOnly cookies in development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import { IConfigEnvironment } from './environment-setup';
 
 export const ENVIRONMENT: IConfigEnvironment = {
   env: 'development',
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: '/api'
 };

921c8f60472fab7055a7da06612bce7e3db44072
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-12 13:55:20 -0400

    feat(web): :sparkles: Add dialog component

---
+++ src/app/shared/types/dialog.ts
@@ -0,0 +1,1 @@
+export type TDialogPosition = 'top' | 'center' | 'bottom';
---
+++ src/app/shared/ui/dialog/dialog.ts
@@ -0,0 +1,45 @@
+import { Component, ElementRef, input, OnInit, viewChild, viewChildren } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroWrenchSolid, heroXMarkSolid } from '@ng-icons/heroicons/solid';
+import { IconButton } from '../icon-button/icon-button';
+import { Card } from '../card/card';
+import { TDialogPosition } from '../../types/dialog';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-dialog',
+  imports: [NgIcon, IconButton, Card, NgClass],
+  templateUrl: './dialog.html',
+  providers: [provideIcons({ heroWrenchSolid, heroXMarkSolid })]
+})
+export class Dialog implements OnInit {
+  // Dialog configuration inputs
+  public readonly position = input.required<TDialogPosition>();
+
+  // Dialog content reference
+  protected readonly dialog = viewChild.required<ElementRef<HTMLDialogElement>>('dialog');
+
+  protected open(): void {
+    this.dialog().nativeElement.showModal();
+  }
+
+  protected close(): void {
+    this.dialog().nativeElement.close();
+  }
+
+  private setupClosers(): void {
+    // Get all elements with data 'close' within the dialog
+    const closers = this.dialog().nativeElement.querySelectorAll('[data-close]');
+
+    // Add click event listeners to each closer element
+    closers.forEach((closer: Element) => {
+      closer.addEventListener('click', () => {
+        this.close();
+      });
+    });
+  }
+
+  public ngOnInit(): void {
+    this.setupClosers();
+  }
+}

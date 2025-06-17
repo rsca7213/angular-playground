0b88e2eb237540c1e63e9cc1d4512ffe270d6531
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-17 12:16:19 -0400

    feat(web): :sparkles: Add findById, update and delete product service endpoints

--- src/app/shared/services/api/products/api-products.ts
+++ src/app/shared/services/api/products/api-products.ts
@@ -7,6 +7,12 @@ import { ENVIRONMENT } from '../../../../../environments/environment';
 import { IApiListProductsQuery } from '../../../dtos/api/products/api-list-products-query';
 import { IApiCreateProductBody } from '../../../dtos/api/products/api-create-product-body';
 import { IApiCreateProductResponse } from '../../../dtos/api/products/api-create-product-response';
+import { IApiUpdateProductBody } from '../../../dtos/api/products/api-update-product-body';
+import { IApiUpdateProductParams } from '../../../dtos/api/products/api-update-product-params';
+import { IApiUpdateProductResponse } from '../../../dtos/api/products/api-update-product-response';
+import { IApiDeleteProductParams } from '../../../dtos/api/products/api-delete-product-params';
+import { IApiFindProductParams } from '../../../dtos/api/products/api-find-product-params';
+import { IApiFindProductResponse } from '../../../dtos/api/products/api-find-product-response';
 
 @Injectable({
   providedIn: 'root'
@@ -23,7 +29,25 @@ export class ApiProducts {
     );
   }
 
+  public findById(params: IApiFindProductParams): Observable<IApiFindProductResponse> {
+    return this.http.get<IApiFindProductResponse>(`${ENVIRONMENT.apiUrl}/products/${params.id}`);
+  }
+
   public create(body: IApiCreateProductBody): Observable<IApiCreateProductResponse> {
     return this.http.post<IApiCreateProductResponse>(`${ENVIRONMENT.apiUrl}/products`, body);
+  }
+
+  public update(
+    body: IApiUpdateProductBody,
+    params: IApiUpdateProductParams
+  ): Observable<IApiUpdateProductResponse> {
+    return this.http.put<IApiUpdateProductResponse>(
+      `${ENVIRONMENT.apiUrl}/products/${params.id}`,
+      body
+    );
+  }
+
+  public delete(params: IApiDeleteProductParams): Observable<void> {
+    return this.http.delete<void>(`${ENVIRONMENT.apiUrl}/products/${params.id}`);
   }
 }
